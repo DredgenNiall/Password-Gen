88c07c5a5dceeb738127c84edb10b833791ad4dc
Dredgen Niall <theoriginalbap@gmail.com>
2021-09-26 14:43:32 -0500

    added user inquiry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,88 @@
 // Assignment code here
 
 
+
+
+
+
+function generatePassword(){
+  passwordLengthFunction();
+};
+
+function passwordLengthFunction(){  
+  var lengthInput = window.prompt("How long should your password be? (numerical value between 8 - 128)");
+  
+  if (lengthInput > 8 && lengthInput < 128){
+    passwordLowerCaseFunction(lengthInput);
+  }
+  else if ( lengthInput < 8){
+    window.alert("Your password is too short!");
+  }
+  else if ( lengthInput > 128)
+  window.alert("Your password is too long!");
+  };
+
+
+    function passwordLowerCaseFunction(lengthInput){  
+      var lowerCaseInput = window.confirm("Should the password include lowercase characters (confirm for Yes, cancel for no)");
+      
+      if (lowerCaseInput === true){
+      passwordUpperCaseFunction(lengthInput, lowerCaseInput);
+
+      }
+      else if ( lowerCaseInput === false) {
+      passwordUpperCaseFunction(lengthInput, lowerCaseInput);
+      };
+    };
+
+    function passwordUpperCaseFunction(lengthInput, lowerCaseInput){  
+      var upperCaseInput = window.confirm("Should the password include uppercase characters (confirm for Yes, cancel for no)");
+      
+      if (upperCaseInput === true){
+        passwordNumericFunction(lengthInput, lowerCaseInput, upperCaseInput);
+
+      }
+      else if ( upperCaseInput === false) {
+        passwordNumericFunction(lengthInput, lowerCaseInput, upperCaseInput);
+      };
+    };
+
+
+    function passwordNumericFunction(lengthInput, lowerCaseInput, upperCaseInput){  
+      var numbersInput = window.confirm("Should the password include numbers (confirm for Yes, cancel for no)");
+      
+      if (numbersInput === true){
+        passwordSpecialFunction(lengthInput, lowerCaseInput, upperCaseInput, numbersInput);
+
+      }
+      else if ( numbersInput === false) {
+        passwordSpecialFunction(lengthInput, lowerCaseInput, upperCaseInput, numbersInput);
+      };
+    };
+
+    function passwordSpecialFunction(lengthInput, lowerCaseInput, upperCaseInput, numbersInput){  
+      var specialInput = window.confirm("Should the password include numbers (confirm for Yes, cancel for no)");
+      
+      if (specialInput === true || specialInput === false ){
+
+        var passwordCriteria = {
+          passwordLength : lengthInput,
+          lower: lowerCaseInput,
+          upper: upperCaseInput,
+          numbers: numbersInput,
+          special: specialInput
+        };
+          createPassword(passwordCriteria);
+      }
+    };
+
+    function createPassword(passwordCriteria){
+    console.log(passwordCriteria);
+
+    
+
+    };
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,10 +90,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var buttonClick = generateBtn.addEventListener("click", writePassword);
+
